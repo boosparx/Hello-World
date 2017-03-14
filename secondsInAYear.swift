bfe107539f179fcdb6ddeb21da77a60897dd67b1
Boo Sparx <tony@tonycaio.com>
2017-03-14 16:32:39 -0400

    Create secondsInAYear.swift
    Homework day #1

---
+++ secondsInAYear.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let sec = 60
+
+let min = 60
+
+let day = 24
+
+print("There are \(sec * min * day) seconds are in a day")
+
+let year = 365
+
+print("There are \(sec * min * day * year) seconds in a year")
